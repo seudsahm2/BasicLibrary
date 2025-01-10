6c4f119a1918de3d9e3bb6c006f341315c6a7eab
seud <seudsahm1@gmail.com>
2025-01-10 21:51:06 +0300

    test added for contents and view

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Book
 # Create your tests here.
+
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="A good title",
+            subtitle="An excellent subtitle",
+            author="Seud Abdulsemed",
+            isbn="12345678909"
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "A good title")
+        self.assertEqual(self.book.subtitle, "An excellent subtitle")
+        self.assertEqual(self.book.author, "Seud Abdulsemed")
+        self.assertEqual(self.book.isbn, "12345678909")
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "excellent subtitle")
+        self.assertTemplateUsed(response, "books/book_list.html")
